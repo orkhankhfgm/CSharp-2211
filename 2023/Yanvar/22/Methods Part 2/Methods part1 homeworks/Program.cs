54730b04160216bd75829513c9cea80b84490bca
Orkhan Farajov <orkhankhf@gmail.com>
2023-01-23 21:00:23 +0400

    Methods part 2

---
+++ 2023/Yanvar/22/Methods Part 2/Methods part1 homeworks/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_part1_homeworks
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             Main methodunda tələbənin ad, soyad, qiymət 1, qiymət 2 və qiymət 3 məlumatları alınıb hamısı bir method'a ötürüləcək.
+             Əlavə olaraq bir Student class'ınız olsun. Bu class'da 3 method olacaq. CalcAverage, RateAverage və bir dənə də CalcAverage'in overload'ı olacaq.
+             Bunun üçün Method Overloading mövzusu araşdırılmalıdır. CalcAverage ortalama qiyməti hesablayacaq. RateAverage isə ortalama qiymətə görə,
+             tələbənin ortalamasının 45-dən yuxarı/aşağı olub-olmamasını check edəcək. Əgər yuxarıdırsa keçib, aşağıdırsa kəsilib yazısını ekrana çıxardacaq.
+             */
+
+            string name = string.Empty, surname = string.Empty;
+            decimal p1 = 0, p2 = 0, p3 = 0;
+
+            Console.WriteLine("Zehmet olmasa telebe melumatlarini daxil edin:");
+
+            Console.WriteLine("Ad:");
+            name = Console.ReadLine();
+
+            Console.WriteLine("Soyad:");
+            surname = Console.ReadLine();
+            
+            Console.WriteLine("Qiymet 1:");
+            p1 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qiymet 2:");
+            p2 = decimal.Parse(Console.ReadLine());
+
+            Console.WriteLine("Qiymet 3:");
+            p3 = decimal.Parse(Console.ReadLine());
+
+            Student student = new Student();
+            student.CalcAverage(name, surname, p1, p2, p3);
+            var test = student.CalcAverage(15, 20, 34);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ 2023/Yanvar/22/Methods Part 2/Methods part1 homeworks/Student.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods_part1_homeworks
+{
+    public class Student
+    {
+        /*
+        Main methodunda tələbənin ad, soyad, qiymət 1, qiymət 2 və qiymət 3 məlumatları alınıb hamısı bir method'a ötürüləcək.
+        Əlavə olaraq bir Student class'ınız olsun. Bu class'da 3 method olacaq. CalcAverage, RateAverage və bir dənə də CalcAverage'in overload'ı olacaq.
+        Bunun üçün Method Overloading mövzusu araşdırılmalıdır. CalcAverage ortalama qiyməti hesablayacaq. RateAverage isə ortalama qiymətə görə,
+        tələbənin ortalamasının 45-dən yuxarı/aşağı olub-olmamasını check edəcək. Əgər yuxarıdırsa keçib, aşağıdırsa kəsilib yazısını ekrana çıxardacaq.
+        */
+        public void CalcAverage(string name, string surname, decimal p1, decimal p2, decimal p3 = 45)
+        {
+            Console.WriteLine("Telebe melumatlari: ");
+            Console.WriteLine("Ad Soyad: {0} {1}", name, surname);
+
+            //(birinci yazi qaydasi)
+            decimal average = CalcAverage(p1, p2, p3);
+            RateAverage(average);
+
+            //(ikinci yazi qaydasi)
+            //RateAverage(CalcAverage(p1, p2, p3));
+        }
+
+        private void RateAverage(decimal avrg)
+        {
+            if(avrg <= 45)
+                Console.WriteLine("Ortalama qiymet : {0} - Imtahandan kesildiniz!", avrg);
+            else
+                Console.WriteLine("Ortalama qiymet : {0} - Imtahandan kecdiniz!", avrg);
+        }
+
+        public decimal CalcAverage(decimal p1, decimal p2, decimal p3)
+        {
+            //(birinci yazi qaydasi)
+            //decimal avrg = (p1 + p2 + p3) / 3;
+            //return avrg;
+
+            return (p1 + p2 + p3) / 3;
+        }
+    }
+}
